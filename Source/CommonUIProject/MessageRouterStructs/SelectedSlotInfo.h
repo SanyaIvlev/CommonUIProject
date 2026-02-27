a179e69cb2eac1fa9a3063e48d7fa11e52717895
Ivlev Oleksandr <81967994+SanyaIvlev@users.noreply.github.com>
2026-02-27 16:26:32 -0600

    Added inventory & gameplay message router plugin

---
+++ Source/CommonUIProject/MessageRouterStructs/SelectedSlotInfo.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Class.h"
+#include "SelectedSlotInfo.generated.h"
+
+class UInventoryItem;
+
+USTRUCT(BlueprintType)
+struct FSelectedSlotInfo
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UInventoryItem* ItemInfo;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bIsItemEquipped;
+};
